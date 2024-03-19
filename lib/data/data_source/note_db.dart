fd12d33b56ade96ca4e66619e297234de822b173
HyungwonJin <hwjin92@gmail.com>
2024-03-19 21:36:13 +0900

    Add NoteDB implementation and update NoteRepositoryImpl

--- lib/data/data_source/note_db.dart
+++ lib/data/data_source/note_db.dart
@@ -1,1 +1,48 @@
-class NoteDB {}
+import 'package:note_app/domain/models/note.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class NoteDB {
+  Database db;
+
+  NoteDB(this.db);
+
+  Future<Note?> getNoteById(int id) async {
+    final List<Map<String, dynamic>> maps = await db.query(
+      'notes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<List<Note>> getNotes() async {
+    final maps = await db.query('notes');
+    return maps.map((e) => Note.fromJson(e)).toList();
+  }
+
+  Future<void> insertNote(Note note) async {
+    await db.insert('notes', note.toJson());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await db.update(
+      'notes',
+      note.toJson(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<void> deleteNoteById(Note note) async {
+    await db.delete(
+      'notes',
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+}
